b1d2c48569df273c8462af3ac232393a71ef8677
D00dlenoodles <RML.D00dlenoodles@gmail.com>
2024-12-08 02:03:51 -0600

    Update to 1.0.11
    Shit seems to work, but only time and testing will tell

--- src/main/java/com/solegendary/reignofnether/registrars/ServerEventRegistrar.java
+++ src/main/java/com/solegendary/reignofnether/registrars/ServerEventRegistrar.java
@@ -2,6 +2,7 @@ package com.solegendary.reignofnether.registrars;
 
 import com.solegendary.reignofnether.attackwarnings.AttackWarningServerEvents;
 import com.solegendary.reignofnether.building.BuildingServerEvents;
+import com.solegendary.reignofnether.config.ConfigVanillaServerEvents;
 import com.solegendary.reignofnether.fogofwar.FogOfWarServerEvents;
 import com.solegendary.reignofnether.gamemode.GameModeServerEvents;
 import com.solegendary.reignofnether.player.PlayerServerEvents;
