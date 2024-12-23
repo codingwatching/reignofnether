7a14eb8f4230596b71a5f311c1837e46eeda3583
SoLegendary <nick.tangson@gmail.com>
2024-12-24 09:47:41 +1100

    ghast height

--- src/main/java/com/solegendary/reignofnether/player/PlayerClientEvents.java
+++ src/main/java/com/solegendary/reignofnether/player/PlayerClientEvents.java
@@ -98,6 +98,7 @@ public class PlayerClientEvents {
                 MC.player.sendSystemMessage(Component.translatable("commands.reignofnether.gamerule.unit_griefing", "/gamerule doUnitGriefing"));
                 MC.player.sendSystemMessage(Component.translatable("commands.reignofnether.gamerule.player_griefing", "/gamerule doSurvivalGriefing"));
                 MC.player.sendSystemMessage(Component.translatable("commands.reignofnether.gamerule.ground_y_level", "/gamerule groundYLevel"));
+                MC.player.sendSystemMessage(Component.translatable("commands.reignofnether.gamerule.flying_max_y_level", "/gamerule flyingMaxYLevel"));
                 MC.player.sendSystemMessage(Component.translatable("commands.reignofnether.gamerule.improved_pathfinding", "/gamerule improvedPathfinding"));
             }
             return 1;
